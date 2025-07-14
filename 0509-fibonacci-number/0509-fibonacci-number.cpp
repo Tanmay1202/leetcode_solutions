7af14171982d6ba191c743501b440e385a005859
Tanmay Singh <singhtanmay1202@gmail.com>
2025-07-14 09:40:59 +0530

    Time: 3 ms (66.64%), Space: 8.2 MB (17.18%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,9 +1,24 @@
 class Solution {
 public:
-    int fib(int n)
-    {
-        if(n<=1) return n;
+    int fib(int n) {
 
-        return fib(n-1) + fib(n-2);
+        if(n<=1)
+        return n;
+
+
+        
+        vector<int> arr(n+1, 0);
+
+        arr[0] = 0;
+        arr[1] = 1;
+
+        for(int i=2; i<n+1; i++)
+        {
+            arr[i] = arr[i-1] + arr[i-2];
+        }
+
+        return arr[n];
+
+        
     }
 };
