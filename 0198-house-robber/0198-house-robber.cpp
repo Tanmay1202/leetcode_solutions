a4cb377e934ee23813fd0925addb9c7f1f020808
Tanmay Singh <singhtanmay1202@gmail.com>
2025-07-14 19:33:53 +0530

    Time: 0 ms (100%), Space: 10.8 MB (22.44%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -3,11 +3,9 @@ public:
     int rob(vector<int>& nums) 
     {
         int n = nums.size();
-        if(n==1)
-        return nums[0];
+        if(n==1) return nums[0];
 
         vector<int> dp(n, 0);
-
         dp[0] = nums[0];
         dp[1] = max(nums[0], nums[1]);
 
@@ -16,7 +14,7 @@ public:
             dp[i] = max(dp[i-1], nums[i] + dp[i-2]);
         }
 
+
         return dp[n-1];
-        
     }
 };
