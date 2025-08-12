8413cb7d115677e85ff3cb0877e1c3f06979ea1e
Tanmay Singh <singhtanmay1202@gmail.com>
2025-08-12 23:00:09 +0530

    Time: 10 ms (98.04%), Space: 11.8 MB (84.31%) - LeetHub

--- 2787-ways-to-express-an-integer-as-sum-of-powers/2787-ways-to-express-an-integer-as-sum-of-powers.cpp
+++ 2787-ways-to-express-an-integer-as-sum-of-powers/2787-ways-to-express-an-integer-as-sum-of-powers.cpp
@@ -1,30 +1,29 @@
 class Solution {
 public:
-    int numberOfWays(int n, int x) 
-    {
-        int bases = floor(pow(n, 1.0/x));
-
-        const int MOD = 1e9 + 7;
-        
-        vector<vector<int>> dp(bases + 1, vector<int>(n+1, 0));
-
-        for(int i=0; i<=bases; i++)
-        {
-            dp[i][0] = 1;
+    // compute a^b, but stop and return limit+1 if it exceeds limit
+    static long long powerLimit(int a, int b, int limit) {
+        long long res = 1;
+        for (int i = 0; i < b; ++i) {
+            if (res > limit / a) return (long long)limit + 1;
+            res *= a;
         }
+        return res;
+    }
 
-        for(int i=1; i<=bases; i++)
-        {
-            for(int j=1; j<=n; j++)
-            {
-                int wt = static_cast<int>(pow(i, x));
-                if(j < wt)
-                dp[i][j] = dp[i-1][j];
-                else
-                dp[i][j] = (dp[i-1][j] + dp[i-1][j - wt]) % MOD;
+    int numberOfWays(int n, int x) {
+        const int MOD = 1000000007;
+        vector<int> dp(n + 1, 0);
+        dp[0] = 1;
+
+        for (int base = 1; ; ++base) {
+            long long wt = powerLimit(base, x, n);
+            if (wt > n) break;
+            for (int j = n; j >= wt; --j) {
+                dp[j] += dp[j - wt];
+                if (dp[j] >= MOD) dp[j] -= MOD;
             }
         }
 
-        return dp[bases][n];
+        return dp[n];
     }
 };
