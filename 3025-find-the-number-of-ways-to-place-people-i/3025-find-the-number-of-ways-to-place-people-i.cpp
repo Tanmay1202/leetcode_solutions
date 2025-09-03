e382c2197eb45bf20cff480627e2d13346a793f9
Tanmay Singh <singhtanmay1202@gmail.com>
2025-09-03 14:52:19 +0530

    Time: 15 ms (39.13%), Space: 32.4 MB (89.13%) - LeetHub

--- 3025-find-the-number-of-ways-to-place-people-i/3025-find-the-number-of-ways-to-place-people-i.cpp
+++ 3025-find-the-number-of-ways-to-place-people-i/3025-find-the-number-of-ways-to-place-people-i.cpp
@@ -2,38 +2,39 @@ class Solution {
 public:
     int numberOfPairs(vector<vector<int>>& points) 
     {
-        int count = 0;
         int n = points.size();
 
-        for(int i = 0; i < n; i++)
-        {
-            for(int j = 0; j < n; j++)
-            {
-                if(i == j) continue;
+        int count = 0;
 
-                // Condition 1: i is upper-left of j
-                if(points[i][0] <= points[j][0] && points[i][1] >= points[j][1] &&
-                   !(points[i][0] == points[j][0] && points[i][1] == points[j][1]))
+        for(int i=0; i<n; i++)
+        {
+            for(int j=0; j<n; j++)
+            {
+                if(i==j) continue;
+
+                if(points[i][0] <= points[j][0] && points[i][1] >= points[j][1] && !(points[i][0] == points[j][0] && points[i][1] == points[j][1]))
                 {
                     bool valid = true;
 
-                    // Check all other points for rectangle condition
-                    for(int k = 0; k < n; k++)
+                    for(int k=0; k<n; k++)
                     {
-                        if(k == i || k == j) continue;
+                        if(i == k || j == k)
+                        continue;
 
-                        if(points[i][0] <= points[k][0] && points[k][0] <= points[j][0] &&
-                           points[j][1] <= points[k][1] && points[k][1] <= points[i][1])
+                        if(points[i][0] <= points[k][0] && points[k][0] <= points[j][0] && points[i][1] >= points[k][1] && points[k][1] >= points[j][1])
                         {
                             valid = false;
                             break;
                         }
                     }
 
-                    if(valid) count++;
+                    if(valid) 
+                    count++;
                 }
+
             }
         }
+
 
         return count;
     }
