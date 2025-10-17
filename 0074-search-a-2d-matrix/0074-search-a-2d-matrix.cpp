b371687f2df5df89e1724fdf8527320a5a88f2f1
Tanmay Singh <singhtanmay1202@gmail.com>
2025-10-17 12:58:34 +0530

    Time: 0 ms (100%), Space: 13.4 MB (7.13%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -19,27 +19,7 @@ public:
             else 
             l = mid + 1;
         }
-        
-    //     for(int i=0; i<s; i++)
-    //     {
-    //         int l=0;
-    //         int r = n-1;
-            
-    //         while(l<=r)
-    //         {
-    //             int mid = l + (r-l)/2;
-    //             if(matrix[i][mid] == target)
-    //             return true;
-    //             if(target>matrix[i][mid])
-    //             l = mid + 1;
-    //             if(target<matrix[i][mid])
-    //             r = mid - 1;
-    //         } 
-    //     }
-       return false;
 
-
-     }
-
-
+        return false;
+    }
 };
