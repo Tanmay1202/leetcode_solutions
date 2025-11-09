100c0fdf9064a129b52b7f6b92018a4e166d9e5f
Tanmay Singh <singhtanmay1202@gmail.com>
2025-11-09 12:20:57 +0530

    Time: 112 ms (15.51%), Space: 140.8 MB (13.5%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -1,43 +1,47 @@
 class Solution {
 private:
-    // Check palindrome using stack but with indices (no substr)
-    bool isPalindrome(const string &s, int left, int right) {
-        stack<char> st;
-
-        for (int i = left; i <= right; i++) {
-            st.push(s[i]);
-        }
-
-        for (int i = left; i <= right; i++) {
-            if (st.top() != s[i])
-                return false;
-            st.pop();
+    bool isPalindrome(string& s, int start, int end)
+    {
+        while(start < end)
+        {
+            if(s[start] == s[end])
+            {
+                start++;
+                end--;
+            }
+            else
+            return false;
         }
 
         return true;
     }
 
-    void backtrack(vector<vector<string>> &result, vector<string> &temp, const string &s, int start) {
-        if (start == s.length()) {
+    void backtrack(string &s, vector<string> temp, vector<vector<string>> &result, int start)
+    {
+        if(start == s.size())
+        {
             result.push_back(temp);
             return;
         }
 
-        for (int end = start; end < s.length(); end++) {
-            if (isPalindrome(s, start, end)) {
-                // Efficient string creation using constructor with start & length
-                temp.push_back(string(s.begin() + start, s.begin() + end + 1));
-                backtrack(result, temp, s, end + 1);
+        for(int i=start; i<s.size(); i++)
+        {
+            if(isPalindrome(s, start, i))
+            {
+                temp.push_back(s.substr(start, i - start + 1));
+                backtrack(s, temp, result, i+1);
                 temp.pop_back();
             }
         }
     }
-
 public:
-    vector<vector<string>> partition(string s) {
-        vector<vector<string>> result;
+    vector<vector<string>> partition(string s) 
+    {
         vector<string> temp;
-        backtrack(result, temp, s, 0);
+        vector<vector<string>> result;
+
+        backtrack(s, temp, result, 0);
+
         return result;
     }
 };
