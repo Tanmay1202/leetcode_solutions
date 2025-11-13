8e5aa22218a5062dc79e93cd90c4651eb4936cc0
Tanmay Singh <singhtanmay1202@gmail.com>
2025-11-14 01:26:33 +0530

    Time: 8 ms (30.66%), Space: 15.9 MB (54.38%) - LeetHub

--- 3228-maximum-number-of-operations-to-move-ones-to-the-end/3228-maximum-number-of-operations-to-move-ones-to-the-end.cpp
+++ 3228-maximum-number-of-operations-to-move-ones-to-the-end/3228-maximum-number-of-operations-to-move-ones-to-the-end.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
-    long long maxOperations(string s) {
-        long long ans = 0;
-        long long ones = 0;
+    int maxOperations(string s) {
+        int ones = 0;
+        int ans = 0;
         int n = s.size();
 
-        for (int i = 0; i < n; ++i) {
-            if (s[i] == '1') {
-                ++ones;
-            } else if (i == n - 1 || s[i + 1] == '1') {
-                // this zero is the last zero of its zero-block
-                ans += ones;
-            }
+        for(int i=0; i<n; i++)
+        {
+            if(s[i] == '1')
+            ones++;
+            else if(i == n-1  || s[i+1] == '1')
+            ans += ones;
         }
+
 
         return ans;
     }
