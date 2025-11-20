70429bc190ca5dc92272c851e2f88e3dd5c9b4ea
Tanmay Singh <singhtanmay1202@gmail.com>
2025-11-20 23:00:47 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -2,22 +2,15 @@ class Solution {
 public:
     int climbStairs(int n) 
     {
-        if(n == 0)
-        return 0;
-
-        if(n == 1)
-        return 1;
-
-        vector<int> dp(n+1, 0);
-
+        vector<int> dp(n);
         dp[0] = 1;
-        dp[1] = 1;
+        dp[1] = 2;
 
-        for(int i=2; i<=n; i++)
+        for(int i=2; i<n; i++)
         {
             dp[i] = dp[i-1] + dp[i-2];
         }
 
-        return dp[n];
+        return dp[n-1];
     }
 };
