08e797df3d85f98e3e86c9b3535a984d75f0565e
Tanmay Singh <singhtanmay1202@gmail.com>
2026-01-02 01:47:04 +0530

    Time: 15 ms (17.8%), Space: 13.5 MB (25.68%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,22 +1,20 @@
 class Solution {
 public:
     int minDistance(string word1, string word2) {
-        
         int n = word1.size();
         int m = word2.size();
 
+
         vector<vector<int>> dp(n+1, vector<int>(m+1, 0));
 
-        
-
-        for(int i=1; i<=n; i++)
+        for(int i=0; i<=n; i++)
         {
             dp[i][0] = i;
         }
 
-        for(int i=1; i<=m; i++)
+        for(int j=1; j<=m; j++)
         {
-            dp[0][i] = i;
+            dp[0][j] = j;
         }
 
         for(int i=1; i<=n; i++)
@@ -32,7 +30,7 @@ public:
 
 
         return dp[n][m];
-
-        
     }
+
+
 };
