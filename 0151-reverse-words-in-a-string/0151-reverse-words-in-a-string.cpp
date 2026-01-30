cf446ba6d26236b932036e4541e2d97d0174197f
Tanmay Singh <singhtanmay1202@gmail.com>
2026-01-30 12:48:25 +0530

    Time: 0 ms (100%), Space: 10 MB (79.43%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -4,12 +4,11 @@ public:
         int n = s.size();
         int i = 0, j = 0;
 
-        // normalize spaces (trim + single internal spaces)
         while (i < n) {
-            while (i < n && s[i] == ' ') i++;   // skip spaces
+            while (i < n && s[i] == ' ') i++;   
             if (i >= n) break;
 
-            if (j > 0) s[j++] = ' ';           // single space between words
+            if (j > 0) s[j++] = ' ';           
 
             while (i < n && s[i] != ' ') {
                 s[j++] = s[i++];
@@ -18,10 +17,10 @@ public:
 
         s.resize(j);
 
-        // reverse entire string
+    
         reverse(s.begin(), s.end());
 
-        // reverse each word
+        
         int start = 0;
         for (int k = 0; k <= j; k++) {
             if (k == j || s[k] == ' ') {
