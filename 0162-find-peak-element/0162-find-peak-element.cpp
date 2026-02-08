5354ef9a0dcc698b7792d85743d4bab387be64a3
Tanmay Singh <singhtanmay1202@gmail.com>
2026-02-08 22:36:03 +0530

    Time: 0 ms (100%), Space: 12.6 MB (26.18%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.cpp
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -8,12 +8,13 @@ public:
         {
             int mid = low + (high - low)/2;
 
-            if(nums[mid] > nums[mid+1])
+            if(nums[mid] > nums[mid + 1])
             high = mid;
             else
             low = mid + 1;
+
         }
 
-        return high;
+        return low;
     }
 };
