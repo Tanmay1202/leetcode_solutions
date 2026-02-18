83c9b37fb1f89b7ec418de71e2cbb8c30934d918
Tanmay Singh <singhtanmay1202@gmail.com>
2026-02-18 21:20:30 +0530

    Time: 0 ms (100%), Space: 29.1 MB (38.51%) - LeetHub

--- 0572-subtree-of-another-tree/0572-subtree-of-another-tree.cpp
+++ 0572-subtree-of-another-tree/0572-subtree-of-another-tree.cpp
@@ -6,25 +6,32 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
-    bool isIdentical(TreeNode* s, TreeNode* t)
-    {
-        if (s == nullptr && t == nullptr) return true;
-        if (s == nullptr || t == nullptr) return false;
+private:
+    bool isIdentical(TreeNode* l, TreeNode* r) {
+        if (!l && !r)
+            return true;
+        if (!l || !r)
+            return false;
 
-        return (s->val == t->val) && isIdentical(s->left, t->left) && isIdentical(s->right, t->right);
+        return l->val == r->val &&
+               isIdentical(l->left, r->left) &&
+               isIdentical(l->right, r->right);
     }
+
 public:
     bool isSubtree(TreeNode* root, TreeNode* subRoot) {
-        if(root == nullptr)
-        return false;
+        if (!root)
+            return false;
 
-        if(isIdentical(root, subRoot))
-        return true;
+        if (isIdentical(root, subRoot))
+            return true;
 
-        return isSubtree(root->left, subRoot) || isSubtree(root->right, subRoot);
+        return isSubtree(root->left, subRoot) ||
+               isSubtree(root->right, subRoot);
     }
 };
