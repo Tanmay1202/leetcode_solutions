3687a1d0159c7926f3da242210ac16a34fd1b4f6
Tanmay Singh <singhtanmay1202@gmail.com>
2026-02-19 16:28:16 +0530

    Time: 7 ms (75.71%), Space: 36.1 MB (39.5%) - LeetHub

---
+++ 1020-number-of-enclaves/1020-number-of-enclaves.cpp
@@ -0,0 +1,54 @@
+class Solution {
+public:
+    int ans = 0;
+    void dfs(vector<vector<int>>& grid, int i, int j, int n, int m)
+    {
+        if(i<0 || j<0 || i>n-1 || j>m-1 || grid[i][j] != 1)
+        return;
+
+        grid[i][j] = 0;
+
+        dfs(grid, i+1, j, n, m);
+        dfs(grid, i, j+1, n, m);
+        dfs(grid, i-1, j, n, m);
+        dfs(grid, i, j-1, n, m);
+    }
+    int numEnclaves(vector<vector<int>>& grid) {
+        int n = grid.size();
+        int m = grid[0].size();
+
+        int count = 0;
+
+        for(int i=0; i<n; i++)
+        {
+            if(grid[i][0] == 1)
+            dfs(grid, i, 0, n, m);
+        }
+        for(int i=0; i<m; i++)
+        {
+            if(grid[0][i] == 1)
+            dfs(grid, 0, i, n, m);
+        }
+        for(int i=0; i<m; i++)
+        {
+            if(grid[n-1][i] == 1)
+            dfs(grid, n-1, i, n, m);
+        }
+        for(int i=0; i<n; i++)
+        {
+            if(grid[i][m-1] == 1)
+            dfs(grid, i, m-1, n, m);
+        }
+
+        for(int i=0; i<n; i++)
+        {
+            for(int j=0; j<m; j++)
+            {
+                if(grid[i][j] == 1)
+                count++;
+            }
+        }
+
+        return count;
+    }
+};
