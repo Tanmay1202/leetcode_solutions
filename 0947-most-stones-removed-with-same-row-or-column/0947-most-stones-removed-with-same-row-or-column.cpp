2e92b54b2859aa8c8f85adcfaf0ee86d65d4d0c0
Tanmay Singh <singhtanmay1202@gmail.com>
2026-04-06 23:32:33 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.cpp
+++ 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.cpp
@@ -1,47 +1,71 @@
-class DSU {
-public:
-    vector<int> parent;
-
-    DSU(int n) {
-        parent.resize(n);
-        for(int i = 0; i < n; i++) parent[i] = i;
-    }
-
-    int find(int x) {
-        if(parent[x] == x) return x;
-        return parent[x] = find(parent[x]);
-    }
-
-    void unite(int x, int y) {
-        int px = find(x);
-        int py = find(y);
-        if(px != py) {
-            parent[px] = py;
-        }
-    }
-};
-
 class Solution {
 public:
+    int findParent(int i, vector<int>& parent)
+    {
+        if(parent[i] == i)
+        return i;
+
+        return parent[i] = findParent(parent[i], parent);
+    }
+
+    void uni(int x, int y, vector<int>& parent, vector<int>& rank)
+    {
+        int px = findParent(x, parent);
+        int py = findParent(y, parent);
+
+        if(rank[px] < rank[py])
+        {
+            parent[px] = py;
+        }
+        else if(rank[py]<rank[px])
+        {
+            parent[py] = px;
+        }
+        else
+        {
+            parent[py] = px;
+            rank[px]++;
+        }
+
+    }
     int removeStones(vector<vector<int>>& stones) {
         int n = stones.size();
-        DSU dsu(n);
+        vector<int> parent;
+        vector<int> rank;
+        parent.resize(n);
+        rank.resize(n);
+        for(int i=0; i<n; i++)
+        {
+            parent[i] = i;
+        }
 
-        for(int i = 0; i < n; i++) {
-            for(int j = i + 1; j < n; j++) {
-                if(stones[i][0] == stones[j][0] || 
-                   stones[i][1] == stones[j][1]) {
-                    dsu.unite(i, j);
+        for(int i=0; i<n; i++)
+        {
+            for(int j=i+1; j<n; j++)
+            {
+                if(stones[i][0] == stones[j][0] || stones[i][1] == stones[j][1])
+                {
+                    uni(i, j, parent, rank);
                 }
             }
         }
 
-        unordered_set<int> components;
-
-        for(int i = 0; i < n; i++) {
-            components.insert(dsu.find(i));
+        for(int i=0; i<n; i++)
+        {
+            parent[i] = findParent(i, parent);
         }
 
-        return n - components.size();
+        int components = 0;
+
+        for(int i=0; i<n; i++)
+        {
+            if(parent[i] == i)
+            components++;
+        }
+
+
+        return n - components;
+
+
     }
 };
