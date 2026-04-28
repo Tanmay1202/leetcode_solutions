5177a4a95facb67791ec44cfb223644b4c51c57b
Tanmay Singh <singhtanmay1202@gmail.com>
2026-04-28 20:48:04 +0530

    Time: 3 ms (28.07%), Space: 17.1 MB (71.38%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.cpp
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.cpp
@@ -6,48 +6,40 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
-private:
-    void helper(vector<vector<int>> &ans, vector<int> &temp, TreeNode* root)
-    {
-        if(!root)
-        return;
+public:
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        vector<vector<int>> result;
+
+        if (!root)
+            return result;
 
         queue<TreeNode*> q;
+
         q.push(root);
 
-        while(!q.empty())
-        {
-            temp.clear();
-            int levelSize = q.size();
-            for(int i=0; i<levelSize; i++)
-            {
-                TreeNode* curr = q.front();
+        while (!q.empty()) {
+            int sz = q.size();
+            vector<int> inter;
+
+            for (int i = 0; i < sz; i++) {
+                TreeNode* top = q.front();
+                inter.push_back(top->val);
                 q.pop();
 
-                temp.push_back(curr->val);
-
-                if(curr->left)
-                q.push(curr->left);
-
-                if(curr->right)
-                q.push(curr->right);
+                if (top->left)
+                    q.push(top->left);
+                if (top->right)
+                    q.push(top->right);
             }
 
-            ans.push_back(temp);
+            result.push_back(inter); 
         }
-    }
-public:
-    vector<vector<int>> levelOrder(TreeNode* root)
-    {
-        vector<vector<int>> ans;
-        vector<int> temp;
 
-        helper(ans, temp, root);
-
-        return ans;
+        return result;
     }
 };
