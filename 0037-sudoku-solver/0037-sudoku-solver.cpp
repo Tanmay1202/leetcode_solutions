bbbb7a06c9e564db8f7f016ab46daacfc5cd6dab
Tanmay Singh <singhtanmay1202@gmail.com>
2026-07-01 14:52:19 +0530

    Time: 243 ms (74.1%), Space: 8.5 MB (99.88%) - LeetHub

--- 0037-sudoku-solver/0037-sudoku-solver.cpp
+++ 0037-sudoku-solver/0037-sudoku-solver.cpp
@@ -1,43 +1,48 @@
 class Solution {
-private:
-    bool validValue(vector<vector<char>>& board, int r, int c, char ch) 
+public:
+    bool validValue(vector<vector<char>> &board, int r, int c, char ch)
     {
-    
-        for (int j = 0; j < 9; j++)
-            if (board[r][j] == ch) return false;
+        for(int i=0; i<9; i++)
+        {
+            if(board[r][i] == ch) return false;
+        }
 
-        
-        for (int i = 0; i < 9; i++)
-            if (board[i][c] == ch) return false;
+        for(int j=0; j<9; j++)
+        {
+            if(board[j][c] == ch) return false;
+        }
 
-        
-        int rowStart = (r/3)*3, colStart = (c/3)*3;
-        for (int i = 0; i < 3; i++)
-            for (int j = 0; j < 3; j++)
-                if (board[rowStart+i][colStart+j] == ch) return false;
+        int rowStart = (r/3)*3; int colStart = (c/3)*3;
+
+        for(int i=0; i<3; i++)
+        {
+            for(int j=0; j<3; j++)
+            {
+                if(board[i + rowStart][j + colStart] == ch) return false;
+            }
+        }
 
         return true;
     }
-
     bool backtrack(vector<vector<char>>& board)
     {
-        for(int i=0; i<board.size(); i++)
+        for(int i=0; i<9; i++)
         {
-            for(int j=0; j<board[0].size(); j++)
+            for(int j=0; j<9; j++)
             {
                 if(board[i][j] != '.')
                 continue;
 
-                for(int value=1; value<=9; value++)
+                for(int val=1; val<=9; val++)
                 {
-                    char ch = value + '0';
-
+                    char ch = val + '0';
                     if(!validValue(board, i, j, ch))
                     continue;
 
                     board[i][j] = ch;
                     if(backtrack(board)) return true;
                     board[i][j] = '.';
+
                 }
 
                 return false;
@@ -47,10 +52,7 @@ private:
 
         return true;
     }
-
-public:
-    void solveSudoku(vector<vector<char>>& board) 
-    {
+    void solveSudoku(vector<vector<char>>& board) {
         backtrack(board);
     }
 };
